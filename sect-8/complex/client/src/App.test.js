9cda8c475e771b2a5b516c28aa750029d58823ef
Finley McIlwaine <finleymcilwaine@gmail.com>
2020-01-21 22:55:17 -0700

    Added complex react app

--- sect-8/complex/client/src/App.test.js
+++ sect-8/complex/client/src/App.test.js
@@ -7,9 +7,3 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
